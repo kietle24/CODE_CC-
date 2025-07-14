7a4c30b800c638b13c396d7f6c5b9ed071969cd9
David_Kiet <leanhkietl366@gmail.com>
2025-07-14 09:06:00 +0700

    nn

---
+++ 1029_líkedlist.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std ;
+    struct ListNode{ 
+     int val ; 
+     ListNode* next ;
+    ListNode(int x): val(x), next(NULL) {}
+     }; 
+ class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int num = 0; 
+        while (head) {
+            num = (num << 1) | head->val; 
+            head = head->next;
+        }
+        return num;
+    }
+};
+int main() { 
+    
+
+}
